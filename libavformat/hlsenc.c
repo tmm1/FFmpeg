1ce48611481b72dd1d3a8808f875e6bcb16af32a
Aman Gupta <aman@tmm1.net>
2017-12-25 11:32:34 -0800

    avformat/hlsenc: return AVERROR_PROTOCOL_NOT_FOUND when http protocol is not available
    Signed-off-by: Aman Gupta <aman@tmm1.net>

--- libavformat/hlsenc.c
+++ libavformat/hlsenc.c
@@ -250,7 +250,7 @@ static int hlsenc_io_open(AVFormatContext *s, AVIOContext **pb, char *filename,
                           AVDictionary **options) {
     HLSContext *hls = s->priv_data;
     int http_base_proto = filename ? is_http_proto(filename) : 0;
-    int err = AVERROR_MUXER_NOT_FOUND;
+    int err = AVERROR_PROTOCOL_NOT_FOUND;
     if (!*pb || !http_base_proto || !hls->http_persistent) {
         err = s->io_open(s, pb, filename, AVIO_FLAG_WRITE, options);
 #if CONFIG_HTTP_PROTOCOL
