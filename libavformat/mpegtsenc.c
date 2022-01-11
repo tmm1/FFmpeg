df168dc10417b76b93381dbae083c5799fa1ea04
Aman Karmani <aman@tmm1.net>
2022-01-11 11:26:32 -0800

    avformat/mpegtsenc: tag AC4 correctly in PMT

--- libavformat/mpegtsenc.c
+++ libavformat/mpegtsenc.c
@@ -491,6 +491,8 @@ static int mpegts_write_pmt(AVFormatContext *s, MpegTSService *service)
         case AVMEDIA_TYPE_AUDIO:
             if (codec_id == AV_CODEC_ID_AC3)
                 put_registration_descriptor(&q, MKTAG('A', 'C', '-', '3'));
+            if (codec_id == AV_CODEC_ID_AC4)
+                put_registration_descriptor(&q, MKTAG('A', 'C', '-', '4'));
             if (codec_id == AV_CODEC_ID_EAC3)
                 put_registration_descriptor(&q, MKTAG('E', 'A', 'C', '3'));
             if (ts->flags & MPEGTS_FLAG_SYSTEM_B) {
